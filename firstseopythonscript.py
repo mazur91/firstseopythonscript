fd9faaa291d3a5aa07e22e8ee480ceedac94b861
mazur91 <michal.mazur@hotmail.com>
2021-10-27 20:38:31 +0200

    Create firstseopythonscript.py

---
+++ firstseopythonscript.py
@@ -0,0 +1,8 @@
+import urllib.request
+ 
+url = "http://www.google.com/ping?sitemap=https://www.example.com/sitemap.xml"
+response = urllib.request.urlopen(url)
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(response.read(), "html.parser")
+print(soup.find("h2").text)
