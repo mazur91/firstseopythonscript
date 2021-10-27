acc0bdd250d71318a779ee5dbab733e78553ef16
mazur91 <michal.mazur@hotmail.com>
2021-10-27 20:37:32 +0200

    Delete fristseopythonscript.py

--- fristseopythonscript.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
- 
-url = "http://www.google.com/ping?sitemap=https://www.example.com/sitemap.xml"
-response = urllib.request.urlopen(url)
-
-from bs4 import BeautifulSoup
-soup = BeautifulSoup(response.read(), "html.parser")
-print(soup.find("h2").text)
-
